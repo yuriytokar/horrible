9b24b46262ee17868036799f9f1e76ecb5be7ee9
yuriytokar <tokars_code@ukr.net>
2023-11-01 17:17:26 +0200

    registr logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import Register from './Pages/register'; 
+import Home from './Pages/home';
 import './styles/App.css'; 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
--- src/Pages/register.js
+++ src/Pages/register.js
@@ -4,39 +4,51 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 const RegisterPage = () => {
-  const [phone, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // const [phone, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+  // const handleUsernameChange = (e) => {
+  //   setUsername(e.target.value);
+  // };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  // const handlePasswordChange = (e) => {
+  //   setPassword(e.target.value);
+  // };
 
-  const handleRegister = async () => {
-    try {
+  // const handleRegister = async () => {
+  //   try {
  
 
-      console.log('Registration successful!', /* response з API */);
-    } catch (error) {
-      console.error('Registration failed', error);
-    }
-  };
+  //     console.log('Registration successful!', /* response з API */);
+  //   } catch (error) {
+  //     console.error('Registration failed', error);
+  //   }
+  // };
+
+
+  const [formData, setFormData] = useState({
+    phone: '',
+    password: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
 
   return (
     <div className="register-container">
       <h2 className="headline">T&S Banking - Реєстрація</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Номер телефону:
           <input
             placeholder="Введіть свій номер телефону"
             type="text"
-            value={phone}
-            onChange={handleUsernameChange}
+            // value={phone}
+            // onChange={handleUsernameChange}
+            onChange={(event) => setFormData({...formData, phone: event.target.value})}
           />
         </label>
         <br />
@@ -45,15 +57,17 @@ const RegisterPage = () => {
           <input
             placeholder="Вигадайте надійний пароль"
             type={isPasswordVisible ? 'text' : 'password'}
-            value={password}
-            onChange={handlePasswordChange}
+            // value={password}
+            // onChange={handlePasswordChange}
+            onChange={(event) => setFormData({...formData, password: event.target.value})}
+
           />
-          <span className="form__eye" onClick={() => setIsPasswordVisible((prev) => !prev)}>
+          {<span className="form__eye" onClick={() => setIsPasswordVisible((prev) => !prev)}>
             {isPasswordVisible ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
-          </span>
+          </span> }
         </label>
         <br />
-        <button type="button" onClick={handleRegister} className="register-button">
+        <button type="submit"  className="register-button" >
           Зареєструватися
         </button>
       </form>
