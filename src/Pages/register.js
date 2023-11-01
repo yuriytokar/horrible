2c7dfd0b611269176411cce8fb9cec4e92e2de88
yuriytokar <tokars_code@ukr.net>
2023-11-01 17:48:42 +0200

    register done

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -4,38 +4,34 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 const RegisterPage = () => {
-  // const [phone, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
-  // const handleUsernameChange = (e) => {
-  //   setUsername(e.target.value);
-  // };
-
-  // const handlePasswordChange = (e) => {
-  //   setPassword(e.target.value);
-  // };
-
-  // const handleRegister = async () => {
-  //   try {
- 
-
-  //     console.log('Registration successful!', /* response з API */);
-  //   } catch (error) {
-  //     console.error('Registration failed', error);
-  //   }
-  // };
-
-
   const [formData, setFormData] = useState({
     phone: '',
-    password: ''
-  })
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
-  }
+    let validationErrors = {};
+
+    if (formData.phone === '' || formData.phone === null) {
+      validationErrors.phone = 'Введіть номер телефону';
+    } else if (formData.phone.length <= 9) {
+      validationErrors.phone = 'Неіснуючий номер телефону';
+    }
+    if (formData.password === '' || formData.password === null) {
+      validationErrors.password = 'Введіть пароль';
+    } else if (formData.password.length < 6) {
+      validationErrors.password = 'Занадто короткий пароль';
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      console.log(formData);
+    }
+  };
 
   return (
     <div className="register-container">
@@ -46,9 +42,9 @@ const RegisterPage = () => {
           <input
             placeholder="Введіть свій номер телефону"
             type="text"
-            // value={phone}
-            // onChange={handleUsernameChange}
-            onChange={(event) => setFormData({...formData, phone: event.target.value})}
+            onChange={(event) =>
+              setFormData({ ...formData, phone: event.target.value })
+            }
           />
         </label>
         <br />
@@ -57,23 +53,25 @@ const RegisterPage = () => {
           <input
             placeholder="Вигадайте надійний пароль"
             type={isPasswordVisible ? 'text' : 'password'}
-            // value={password}
-            // onChange={handlePasswordChange}
-            onChange={(event) => setFormData({...formData, password: event.target.value})}
-
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })
+            }
           />
-          {<span className="form__eye" onClick={() => setIsPasswordVisible((prev) => !prev)}>
+          <span className="form__eye" onClick={() => setIsPasswordVisible((prev) => !prev)}>
             {isPasswordVisible ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
-          </span> }
+          </span>
         </label>
         <br />
-        <button type="submit"  className="register-button" >
+        <button type="submit" className="register-button">
           Зареєструватися
         </button>
       </form>
       <p className="login-note">
         Вже маєте акаунт? <Link to="/">Увійдіть</Link>
       </p>
+      <span className="text-danger">
+        {errors.phone && `${errors.phone}; `} {errors.password && errors.password}
+      </span>
     </div>
   );
 };
