6227e8f10dc4b39f377b3a9848eef0a9399562e6
Oleh-Stefaniv <olegstefaniv11@gmail.com>
2023-12-18 16:05:49 +0200

    add some code in DiagramPage

--- src/Pages/DiagramPage.js
+++ src/Pages/DiagramPage.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../styles/DiagramPage.css';
 
 function DiagramPage() {
-  const [activeTab, setActiveTab] = useState('tab1');
+  const [activeTab, setActiveTab] = useState('Statistics');
+  const [operations, setOperations] = useState([]);
+
+  useEffect(() => {
+    const fetchOperations = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/operations');
+        setOperations(response.data);
+      } catch (error) {
+        console.error('Error fetching operations', error);
+      }
+    };
+    fetchOperations();
+  }, []);
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
 
   return (
-    <div>
+    <div className="mainDia">
       <nav>
         <ul>
           <li className={activeTab === 'Statistics' ? 'active' : ''} onClick={() => handleTabChange('Statistics')}>
@@ -25,11 +39,34 @@ function DiagramPage() {
       </nav>
 
       {activeTab === 'Statistics' && (
-        <div>
-          {/* Вміст для вкладки Statistics */}
-          <h2>Content for Statistics</h2>
+        <div className="diagram-content">
+          <div className="firstline">
+            <div className="diagram-window transferStatistics">
+              <h2>Transfer Statistics</h2>
+              <p>Amount: {operations[0].amount}</p>
+              <p>Count: {operations[0].count}</p>
+            </div>
+            <div className="diagram-window top-upStatistics">
+              <h2>Top-up Statistics</h2>
+              <p>Amount: {operations[1].amount}</p>
+              <p>Count: {operations[1].count}</p>
+            </div>
+          </div>
+          <div className="secondline">
+            <div className="diagram-window depositStatistics">
+              <h2>Deposit Statistics</h2>
+              <p>Amount: {operations[2].amount}</p>
+              <p>Count: {operations[2].count}</p>
+            </div>
+            <div className="diagram-window withdrawStatistics">
+              <h2>Withdraw Statistics</h2>
+              <p>Amount: {operations[3].amount}</p>
+              <p>Count: {operations[3].count}</p>
+            </div>
+          </div>
         </div>
       )}
+
 
       {activeTab === 'Graphs' && (
         <div>
