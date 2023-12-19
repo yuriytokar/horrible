4deee247fec67a10ffc08e971751df31a76ae6c3
Oleh-Stefaniv <olegstefaniv11@gmail.com>
2023-12-19 20:49:31 +0200

    add Diagrams

--- src/Pages/DiagramPage.js
+++ src/Pages/DiagramPage.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+// eslint-disable-next-line
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 import '../styles/DiagramPage.css';
 
 function DiagramPage() {
-  const [activeTab, setActiveTab] = useState('Diagram');
+  const [activeTab, setActiveTab] = useState('Statistics');
   const [operations, setOperations] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     const fetchOperations = async () => {
@@ -21,6 +23,67 @@ function DiagramPage() {
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
+  };
+
+  if (operations.length === 0) {
+    return <p>Loading...</p>; 
+  }
+
+  const data01 = [
+    { name: operations[0].type, value: operations[0].used_money },
+    { name: operations[1].type, value: operations[1].used_money },
+    { name: operations[2].type, value: operations[2].used_money },
+    { name: operations[3].type, value: operations[3].used_money },
+  ];
+
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+  
+    return (
+      <g className='g1'>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+          {payload.name}
+        </text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#ffffff">{`UM ${value}`}</text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+          {`(Rate ${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
   };
 
   return (
@@ -101,9 +164,35 @@ function DiagramPage() {
       {activeTab === 'Diagram' && (
         <div className='linediagram'>
           <div className='firstdiagram'>
+            <PieChart width={600} height={400}>
+              <Pie
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                data={data01}
+                cx="50%"
+                cy="50%"
+                innerRadius={80}
+                outerRadius={120}
+                fill="#8884d8"
+                dataKey="value"
+                onMouseEnter={onPieEnter}
+              />
+            </PieChart>
           </div>
           <div className='seconddiagram'>
-            {/* Додайте інші елементи або графіки для другої секції, якщо необхідно */}
+            <PieChart width={600} height={400}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={data01}
+                cx="50%"
+                cy="50%"
+                outerRadius={120}
+                fill="#8884d8"
+                label
+              />
+              <Tooltip />
+            </PieChart>
           </div>
         </div>
       )}
